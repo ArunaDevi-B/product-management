12b4e5f4815a014851c6f7783d6201355ec38eb2
LAPTOP-MAGEPN8V\redmi <arunadevi.b@technotackle.com>
2024-06-10 17:50:14 +0530

    integration implemented

--- client/src/components/DataTable.tsx
+++ client/src/components/DataTable.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
-import { Table, Modal, Form, Input, Space, Tag, Popconfirm, Button } from 'antd';
+import React, { useState, useEffect } from 'react';
+import { Table, Modal, Form, Input, Space, Tag, Popconfirm, Button, message } from 'antd';
 import type { TableProps } from 'antd';
 import { AiOutlineEdit, AiOutlineDelete } from 'react-icons/ai';
+import axios from 'axios';
 import '../../public/table.css';
 
 interface DataType {
@@ -12,68 +13,72 @@ interface DataType {
   quantity: number;
 }
 
-const initialData: DataType[] = [
-  {
-    key: '1',
-    product_name: 'Laptop',
-    price: 32,
-    number: 601,
-    quantity: 0,
-  },
-  {
-    key: '2',
-    product_name: 'Desktop',
-    price: 42,
-    number: 602,
-    quantity: 4,
-  },
-  {
-    key: '3',
-    product_name: 'Mac',
-    price: 32,
-    number: 603,
-    quantity: 10,
-  },
-  {
-    key: '4',
-    product_name: 'Laptop',
-    price: 32,
-    number: 601,
-    quantity: 0,
-  },
-  {
-    key: '5',
-    product_name: 'Desktop',
-    price: 0,
-    number: 602,
-    quantity: 4,
-  },
-  {
-    key: '6',
-    product_name: 'Mac',
-    price: 32,
-    number: 603,
-    quantity: 10,
-  },
-];
-
-
 const DataTable: React.FC = () => {
-    const [data, setData] = useState<DataType[]>(initialData);
+    const [data, setData] = useState<DataType[]>([]);
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [editingRecord, setEditingRecord] = useState<DataType | null>(null);
+  const [isAddModalVisible, setIsAddModalVisible] = useState(false);
   const [form] = Form.useForm();
+  const [addForm] = Form.useForm();
 
+  useEffect(() => {
+    fetchData()
+  }, []);
+  const fetchData = () => {
+        // Fetch data from API when component mounts
+        axios.get('http://localhost:9000/products')
+        .then(response => {
+          setData(response.data);
+        })
+        .catch(error => {
+          message.error('Error deleting record');
+          console.error('Error fetching data:', error);
+        });
+  }
+
+  const handleAddProduct = () => {
+    addForm.validateFields().then(values => {
+      axios.post('http://localhost:9000/products', values)
+        .then(response => {
+          if(response?.data?.message){
+            message.error(response?.data?.message);
+          }else{
+            message.success('Product added successfully');
+            addForm.resetFields();
+            console.log('Product added successfully:', response.data);
+          }
+          fetchData();
+        })
+        .catch(error => {
+          message.error('Error adding product');
+          console.error('Error adding product:', error);
+        });
+      setIsAddModalVisible(false);
+    });
+  };
   
+  const handleAddCancel = () => {
+    setIsAddModalVisible(false);
+    addForm.resetFields();
+  };
+
   const handleEdit = (record: DataType) => {
-    console.log(record,'record');
     setEditingRecord(record);
     form.setFieldsValue(record);
     setIsModalVisible(true);
   };
 
-  const handleDelete = (key: string) => {
-    setData(data.filter(item => item.key !== key));
+  const handleDelete = (number: number) => {
+    axios.delete(`http://localhost:9000/products/${number}`)
+    .then(response => {
+      console.log('Record deleted successfully:', response.data);
+      message.success('Record deleted successfully');
+      fetchData();
+    })
+    .catch(error => {
+      message.error('Error deleting record');
+      console.error('Error deleting record:', error);
+    });
   };
 
 
@@ -82,7 +87,6 @@ const columns: TableProps<DataType>['columns'] = [
     title: 'Product Name',
     dataIndex: 'product_name',
     key: 'product_name',
-    // render: (text) => <a>{text}</a>,
   },
   {
     title: 'Price',
@@ -119,7 +123,7 @@ const columns: TableProps<DataType>['columns'] = [
         <div onClick={() => handleEdit(record)}><AiOutlineEdit /></div>
         <Popconfirm
           title="Are you sure to delete this record?"
-          onConfirm={() => handleDelete(record.key)}
+          onConfirm={() => handleDelete(record.number)}
           okText="Yes"
           cancelText="No"
         >
@@ -132,7 +136,16 @@ const columns: TableProps<DataType>['columns'] = [
 
 const handleOk = () => {
       form.validateFields().then(values => {
-        setData(data.map(item => (item.key === editingRecord?.key ? { ...item, ...values } : item)));
+        axios.put(`http://localhost:9000/products/${values.number}`, values)
+        .then(response => {
+          message.success('Product updated successfully');
+          console.log('Product updated successfully:', response.data);
+          fetchData(); // Fetch updated data after successful update
+        })
+        .catch(error => {
+          message.error('Error updating data');
+          console.error('Error updating data:', error);
+        });
         setIsModalVisible(false);
         setEditingRecord(null);
         form.resetFields();
@@ -145,9 +158,20 @@ const handleOk = () => {
       form.resetFields();
     };
 
+    const positiveNumberRule = {
+      validator: (_: any, value: number) => {
+        if (value <= 0) {
+          return Promise.reject(new Error('Value cannot be less than zero'));
+        }
+        return Promise.resolve();
+      },
+    };
+
   return (
   <div className="data-table-container">
-    <div className="data-table-title">Available Products</div>
+    <div className="data-table-title">Available Products<Button type="primary" onClick={() => setIsAddModalVisible(true)} style={{ float: 'right', backgroundColor: '#fafafa', borderColor: 'black', color: 'black' }}>
+         + Add New
+        </Button></div>
     <Table
       columns={columns}
       dataSource={data}
@@ -155,22 +179,43 @@ const handleOk = () => {
       pagination={{ pageSize: 7 }}
     />
      <Modal
-        title="Edit Record"
+        title="Edit Product"
         visible={isModalVisible}
         onOk={handleOk}
         onCancel={handleCancel}
       >
         <Form form={form} layout="vertical">
-          <Form.Item name="product_name" label="Product Name" rules={[{ required: true, message: 'Please enter the name' }]}>
+          <Form.Item name="product_name" label="Product Name" rules={[{ required: true, message: 'Please enter the Product name' }]}>
             <Input />
           </Form.Item>
-          <Form.Item name="price" label="Price" rules={[{ required: true, message: 'Please enter the age' }]}>
+          <Form.Item name="price" label="Price" rules={[{ required: true, message: 'Please enter the Price' }, positiveNumberRule]}>
             <Input type="number" />
           </Form.Item>
-          <Form.Item name="number" label="Product Number" rules={[{ required: true, message: 'Please enter the address' }]}>
+          <Form.Item name="number" label="Product Number" rules={[{ required: true, message: 'Please enter the product number' }, positiveNumberRule]}>
+            <Input disabled  type="number"/>
+          </Form.Item>
+          <Form.Item name="quantity" label="Quantity" rules={[{ required: true, message: 'Please enter the quantity' }, positiveNumberRule]}>
+            <Input  type="number"/>
+          </Form.Item>
+        </Form>
+      </Modal>
+      <Modal
+        title="Add New Product"
+        visible={isAddModalVisible}
+        onOk={handleAddProduct}
+        onCancel={handleAddCancel}
+      >
+        <Form form={addForm} layout="vertical">
+          <Form.Item name="product_name" label="Product Name" rules={[{ required: true, message: 'Please enter the Product name' }]}>
             <Input />
           </Form.Item>
-          <Form.Item name="quantity" label="Quantity" rules={[{ required: true, message: 'Please enter the address' }]}>
+          <Form.Item name="price" label="Price" rules={[{ required: true, message: 'Please enter the price' }]}>
+            <Input type="number" />
+          </Form.Item>
+          <Form.Item name="number" label="Product Number" rules={[{ required: true, message: 'Please enter the product number' }]}>
+            <Input />
+          </Form.Item>
+          <Form.Item name="quantity" label="Quantity" rules={[{ required: true, message: 'Please enter the quantity' }]}>
             <Input />
           </Form.Item>
         </Form>
